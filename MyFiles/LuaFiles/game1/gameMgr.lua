c03557171fe2218169ee1d2510527298d58481f5
Sky.Allen <894982165@qq.com>
2024-06-09 14:08:04 +0800

    3

--- MyFiles/LuaFiles/game1/gameMgr.lua
+++ MyFiles/LuaFiles/game1/gameMgr.lua
@@ -2,9 +2,22 @@
 local gameMgr=class("gameMgr");
 
 function gameMgr:ctor(a)
+   --print("ctor",a);
+    ---@UnityEngine.GameObject
+    self.cube=UnityEngine.GameObject.Find("Cube");
+    print(self.cube);
+end
+
+function gameMgr:ggk()
+
 end
 
 function gameMgr:update()
+    ---@type UnityEngine.Transform
+    local t =self.cube:GetComponent(typeof(UnityEngine.Transform));
+    t.localPosition=UnityEngine.Vector3(math.random(1,3),math.random(1,3),0);
+
+    --print(t);
 end
 
 return gameMgr;
