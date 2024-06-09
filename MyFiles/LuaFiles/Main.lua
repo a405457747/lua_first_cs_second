a1d7dad569429cb2146802d52ca7655a149fe8a0
Sky.Allen <894982165@qq.com>
2024-06-09 10:06:32 +0800

    3

--- MyFiles/LuaFiles/Main.lua
+++ MyFiles/LuaFiles/Main.lua
@@ -1,11 +1,6 @@
 local Main={};
 
-local function test()
-    local a=3;
-    a=a+5;
-    a=a*5;
-    return a;
-end;
+
 
 local function init()
     print("Main init lua version ",_VERSION)
@@ -16,9 +11,9 @@ local function init()
 
     UnityEngine= CS.UnityEngine;
 
-    dbg=0;
+    dbg=1;
     if(dbg==1)then
-        package.cpath = package.cpath .. ';C:/Users/justi/.IntelliJIdea2019.3/config/plugins/intellij-emmylua/classes/debugger/emmy/windows/x64/?.dll';
+        package.cpath = package.cpath .. ';C:/Users/justi/.IntelliJIdea2019.3/config/plugins/intellij-emmylua/classes/debugger/emmy/windows/x64/?.dll'
         dbg = require('emmy_core')
         dbg.tcpConnect('localhost', 9966)
     end
@@ -34,7 +29,6 @@ local function init()
 end
 
 
-test();
 init();
 
 return Main;
--- MyFiles/LuaFiles/game1/gameMgr.lua
+++ MyFiles/LuaFiles/game1/gameMgr.lua
@@ -2,6 +2,25 @@ local gameMgr=class("gameMgr");
 
 function gameMgr:start()
     print("i gam game1 gameMgr");
+    self.k=555;
+    gameMgr.k=333;
+end
+
+local function test(self)
+
+    local a=3;
+    a=a+self.k;
+    a=a*5;
+
+    return a;
+
+end;
+
+function gameMgr:update()
+    dbg.breakHere()
+    test(gameMgr);
+    print("ggk");
+
 end
 
 return gameMgr;
