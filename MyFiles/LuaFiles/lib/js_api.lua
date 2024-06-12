233695a27a2fe28c680e9a586a8b1cda0e99e922
Sky.Allen <894982165@qq.com>
2024-06-12 09:27:46 +0800

    2

--- MyFiles/LuaFiles/lib/js_api.lua
+++ MyFiles/LuaFiles/lib/js_api.lua
@@ -193,6 +193,52 @@ function js.join(arr,flag)
     return table.concat(arr,flag);
 end
 
+function string.serialize(obj)
+    local lua = ""
+    local t = type(obj)
+    if t == "number" then
+        lua = lua .. obj
+    elseif t == "boolean" then
+        lua = lua .. tostring(obj)
+    elseif t == "string" then
+        lua = lua .. string.format("%q", obj)
+    elseif t == "table" then
+        lua = lua .. "{\n"
+        for k, v in pairs(obj) do
+            lua = lua .. "[" .. string.serialize(k) .. "]=" .. string.serialize(v) .. ",\n"
+        end
+        local metatable = getmetatable(obj)
+        if metatable ~= nil and type(metatable.__index) == "table" then
+            for k, v in pairs(metatable.__index) do
+                lua = lua .. "[" .. string.serialize(k) .. "]=" .. string.serialize(v) .. ",\n"
+            end
+        end
+        lua = lua .. "}"
+    elseif t == "nil" then
+        return nil
+    else
+        error("can not serialize a " .. t .. " type.")
+    end
+    return lua
+end
+
+function string.unserialize(lua)
+    local t = type(lua)
+    if t == "nil" or lua == "" then
+        return nil
+    elseif t == "number" or t == "string" or t == "boolean" then
+        lua = tostring(lua)
+    else
+        error("can not unserialize a " .. t .. " type.")
+    end
+    lua = "return " .. lua
+    local func = loadstring(lua);
+    if func == nil then
+        return nil
+    end
+    return func()
+end
+
 local function test()
     local a={3,4,5,6,7,9};
     --local b =js.splice(a,2,2);--3679;
